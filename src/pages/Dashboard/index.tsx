ddaeda0b6aea2ec867b81b2be33dfac5df16501f
Mayara Alencar <mayara.rma@gmail.com>
2020-05-03 16:25:44 -0300

    axios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,28 +28,6 @@ const Dashboard: React.FC = () => {
           </div>
           <FiChevronRight size={20} />
         </a>
-        <a href="teste" >
-          <img
-            src="https://avatars1.githubusercontent.com/u/37077860?s=460&u=f51546b77ac8f04254de5373a7c9a3fd3b537fc3&v=4"
-            alt="Mayara Alencar"
-          />
-          <div>
-            <strong>rockeseat/unform</strong>
-            <p>Easy peasy highly scalable</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste" >
-          <img
-            src="https://avatars1.githubusercontent.com/u/37077860?s=460&u=f51546b77ac8f04254de5373a7c9a3fd3b537fc3&v=4"
-            alt="Mayara Alencar"
-          />
-          <div>
-            <strong>rockeseat/unform</strong>
-            <p>Easy peasy highly scalable</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
     </>)
 };
